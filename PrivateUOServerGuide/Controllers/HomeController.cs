e213d38050660f44f0d52230297168538120a55f
Eric J <eaj1990@outlook.com>
2021-01-22 15:27:21 -0500

    ASP.NET addition

---
+++ PrivateUOServerGuide/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrivateUOServerGuide.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Want your own private UO Server to experiment on?";
+            return View();
+        }
+
+        public ActionResult Step1()
+        {
+            ViewBag.Message = "Step 1";
+
+            return View();
+        }
+
+        public ActionResult Step2()
+        {
+            ViewBag.Message = "Step 2";
+            return View();
+        }
+
+        public ActionResult Step3()
+        {
+            ViewBag.Message = "Step 3";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PrivateUOServerGuide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrivateUOServerGuide.MvcApplication" Language="C#" %>
